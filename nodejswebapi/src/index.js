672a31f614b762774a65140cc8f2b24f9398d6d9
aunkittiphop <kittipop5.1@hotmail.com>
2023-02-22 19:53:38 +0700

    22/2/2023

--- nodejswebapi/src/index.js
+++ nodejswebapi/src/index.js
@@ -362,7 +362,7 @@ app.get("/getResearch", jsonParser, function (req, res) {
 
 app.put("/HistoryDiseaseModify", jsonParser, function (req, res, next) {
   //   console.log(req.body);
-  let imagelink = "http://127.0.0.1:3032/image/" + req.body.ImageNameUpdate;
+  let imagelink = "http://127.0.0.1:3002/image/" + req.body.ImageNameUpdate;
   database.getConnection(function (err, connection) {
     if (err) {
       //   console.log(err);
