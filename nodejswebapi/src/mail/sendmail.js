179cfe9e3dbdf2406d30c87597a109fcce9b59ae
Chonlatee Khamlue <46771457+chonlatee11@users.noreply.github.com>
2023-03-22 23:54:59 +0700

    Update sendmail.js

--- nodejswebapi/src/mail/sendmail.js
+++ nodejswebapi/src/mail/sendmail.js
@@ -14,7 +14,7 @@ export const sendMailAdmin = (emailDestination, res) => {
     from: process.env.EMAIL_USERNAME,
     to: emailDestination,
     subject: "verify email",
-    html: `กรุณาคลิ๊ก <a href="${IP}/verify/admin${token}">ที่นี่</a> เพื่อยืนยันอีเมล์`,
+    html: `กรุณาคลิ๊ก <a href="${IP}:3000/verify/admin${token}">ที่นี่</a> เพื่อยืนยันอีเมล์`,
   };
 
   transporter.sendMail(mailOptions, function (error, info) {
@@ -38,7 +38,7 @@ export const sendMailResearch = (emailDestination, res) => {
     from: process.env.EMAIL_USERNAME,
     to: emailDestination,
     subject: "verify email",
-    html: `กรุณาคลิ๊ก <a href="${IP}/verify/research${token}">ที่นี่</a> เพื่อยืนยันอีเมล์`,
+    html: `กรุณาคลิ๊ก <a href="${IP}:3000/verify/research${token}">ที่นี่</a> เพื่อยืนยันอีเมล์`,
   };
 
   transporter.sendMail(mailOptions, function (error, info) {
